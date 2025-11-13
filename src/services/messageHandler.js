5969277400e347a1a4269a4dc223107e24f4bfbb
Carlitos Vargas <carlos.vr56@gmail.com>
2025-11-12 23:20:40 -0300

    feat: add and implemented menu and interactive message type

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -8,6 +8,7 @@ class MessageHandler {
 
       if (this.isGreetingMessage(incomingMessageIsGreeting)) {
         await this.sendWelcomeMessage(message.from, message.id, senderInfo);
+        await this.sendWelcomeMenu(message.from);
       } else {
         const response = `Echo: ${message.text.body}`;
         await whatsappService.sendMessage(message.from, response, message.id);
@@ -26,9 +27,29 @@ class MessageHandler {
   }
 
   async sendWelcomeMessage(to, messageId, senderInfo) {
-    const name = this.getSenderName(senderInfo); 
+    const name = this.getSenderName(senderInfo);
     const welcomeMessage = `Bienvenido a nuestro servicio de Delivery de Fitoxpress, ${name}`;
     await whatsappService.sendMessage(to, welcomeMessage, messageId);
+  }
+
+  async sendWelcomeMenu(to) {
+    const menuTitle = '¿En qué te podemos ayudar?';
+    const menuButtons = [
+      {
+        type: 'reply',
+        reply: { id: 'option_1', title: 'Agendar' }
+      },
+      {
+        type: 'reply',
+        reply: { id: 'option_2', title: 'Consutar' }
+      },
+      {
+        type: 'reply',
+        reply: { id: 'option_3', title: 'Ubicación' }
+      },
+    ];
+
+    await whatsappService.sendInteractiveButton(to, menuTitle, menuButtons);
   }
 }
 
